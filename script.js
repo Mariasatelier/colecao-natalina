3d7399f27ee6409b860a7698ce966705562fd3a8
Mariasatelier <91706311+Mariasatelier@users.noreply.github.com>
2022-02-09 11:25:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+var itemCmp = document.querySelector('.item-content');
+var imgItem = document.querySelector('.img-item');
+var titleItem = document.querySelector('.title-item');
+var valorItem = document.querySelector('.valor-item');
+var pacelaItem = document.querySelector('.pacela-item');
+var descricaoItem = document.querySelector('.descricao-item');
+var bxX = document.querySelector('.bx-x');
+var cdp = document.querySelector('.cdp');
+var body = document.querySelector('body');
+var item1 = document.querySelector('.item-1');
+var item2 = document.querySelector('.item-2');
+var item3 = document.querySelector('.item-3');
+var item4 = document.querySelector('.item-4');
+var item5 = document.querySelector('.item-5');
+var item6 = document.querySelector('.item-6');
+var item7 = document.querySelector('.item-7');
+var item8 = document.querySelector('.item-8');
+
+item1.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/0yGSWbBH/Whats-App-Image-2022-01-26-at-16-36-33-1-removebg-preview.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa natalina branca com tecido 100% algodão"
+  valorItem.innerHTML = "R$17,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com estampa natalina branca para enfeitar sua mesa."
+  cdp.value = "UKYU"
+});
+
+item2.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/6Q9vDBs6/Whats-App-Image-2022-01-26-at-16-36-33-2-removebg-preview.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa natalina vermelha com tecido 100% algodão"
+  valorItem.innerHTML = "R$17,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com estampa natalina vermelha para enfeitar sua mesa."
+  cdp.value = "JYGY"
+});
+
+item3.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/Hxn7k3sf/Whats-App-Image-2022-01-26-at-16-36-33-3-removebg-preview.png"
+  titleItem.innerHTML = "Capa de sousplat com estampa natalina verde com tecido de oxford"
+  valorItem.innerHTML = "R$14,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa de sousplat com estampa natalina verde para enfeitar sua mesa."
+  cdp.value = "JSUS"
+});
+
+item4.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/0jvLBYDB/Whats-App-Image-2022-01-27-at-18-13-05-4-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em anel com presentinho"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em anel com presentinho para endeitar seu guardanapo."
+  cdp.value = "AVAN"
+});
+
+item5.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/qMThJLMh/Whats-App-Image-2022-01-27-at-18-13-01-3-removebg-preview.png"
+  titleItem.innerHTML = "Capa para botijão com estampa natalina branca"
+  valorItem.innerHTML = "R$18,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Capa para botijão com estampa natalina branca para embelezar sua cozinha."
+  cdp.value = "OCIH"
+});
+
+item6.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/nr4MwmwR/Whats-App-Image-2022-01-27-at-18-13-01-2-removebg-preview.png"
+  titleItem.innerHTML = "Pano de fogão com estampa natalina branca e vermelha"
+  valorItem.innerHTML = "R$18,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Pano de fogão com estampa natalina branca e vermelha para deixar sua cozinha mais bonita."
+  cdp.value = "AAA3"
+});
+
+item7.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/9Xt3qkj5/Whats-App-Image-2022-01-27-at-18-13-06-1-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina azul e branca"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina azul e branca pare enfeitar seu guardanapo."
+  cdp.value = "EAEA"
+});
+ 
+item8.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/d08Kn8rd/Whats-App-Image-2022-01-27-at-18-13-06-2-removebg-preview.png"
+  titleItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina vermelha HOHO"
+  valorItem.innerHTML = "R$8,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Prendedor de guardanapo em laço com estampa natalina vermelha HOHO para enfeitar seu guardanapo."
+  cdp.value = "KYGY"
+});
